fdb4005c36de3e91bedc0ebddcd818778a036484
Kenan Ben <gazokenan@gmail.com>
2024-12-04 21:28:51 +0800

    initial-nav-contents

---
+++ app/driversProfile/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../page.module.css';
+
+export default function DriversPage() {
+  return <div className={styles.page}>Drivers Page</div>
+}
---
+++ app/payment/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../page.module.css';
+
+export default function PaymentPage() {
+  return <div className={styles.page}>Payment Page</div>
+}
