8cd760e72d028904027e6f78a0041049c04da9d9
Kenan Ben <gazokenan@gmail.com>
2024-12-04 16:33:00 +0800

    login-layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import styles from '../page.module.css';
 
 export default function LoginPage() {
-  return (<div className={styles.page}>Login Page</div>
+  return (
+  <div className={styles.page}>
+    <h1>Login</h1>
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" required />
+        </div>
+        <button type="submit" className="submit-button">Login</button>
+      </form>
+    </div>
   )
 }
