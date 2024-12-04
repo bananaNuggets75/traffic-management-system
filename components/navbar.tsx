a5000b54dc671ffa840304c632eb7372b9499e01
Kenan Ben <gazokenan@gmail.com>
2024-12-04 21:48:36 +0800

    update-nav-design

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,43 +4,30 @@ import React from 'react';
 export default function Navbar() {
   return (
     <header>
-      <nav className="flex items-center justify-between p-6 lg:px-8 h-20 border border-t-0 border-l-0 border-r-0 border-b-gray-600">
+      <nav>
         <div className="flex lg:flex-1">
-          <Link href="/" className="m-1.5 p-1.5 text-lg font-bold text-gray-800">
+          <Link href="/" className="text-lg font-bold text-gray-800">
             Next.js Authentication
           </Link>
         </div>
-
-        <ul className="flex items-center space-x-6 text-sm font-medium text-gray-600">
+        <ul>
           <li>
-            <Link href="/dashboard" className="m-1.5 p-1.5 hover:text-black">
-              Dashboard
-            </Link>
+            <Link href="/dashboard">Dashboard</Link>
           </li>
           <li>
-            <Link href="/payment" className="m-1.5 p-1.5 hover:text-black">
-              Fine Management and Payment
-            </Link>
+            <Link href="/payment">Fine Management and Payment</Link>
           </li>
           <li>
-            <Link href="/driversProfile" className="m-1.5 p-1.5 hover:text-black">
-              Drivers Profile and Violation History
-            </Link>
+            <Link href="/driversProfile">Drivers Profile and Violation History</Link>
           </li>
           <li>
-            <Link href="/violationReporting" className="m-1.5 p-1.5 hover:text-black">
-              Violation and Reporting
-            </Link>
+            <Link href="/violationReporting">Violation and Reporting</Link>
           </li>
         </ul>
-
-        <div className="flex items-center">
-          <Link
-            href="/login"
-            className="m-1.5 p-2 text-sm font-semibold text-white bg-green-500 rounded hover:bg-green-600"
-          >
-            Log In
-          </Link>
+        <div className="right-nav">
+          <button className="login-button">
+            <Link href="/login">Log In</Link>
+          </button>
         </div>
       </nav>
     </header>
