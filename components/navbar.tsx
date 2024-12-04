0acb46744544cdecb8049855e4890e24d983a123
Kenan Ben <gazokenan@gmail.com>
2024-12-04 21:31:30 +0800

    link-nav-pages

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,6 +20,15 @@ export default function Navbar() {
         <Link href="/login" className="m-1.5 p-1.5">
           Log In
         </Link>
+        <Link href="/payment" className="m-1.5 p-1.5">
+          Fine Management and Payment
+        </Link>
+        <Link href="/driversProfile" className="m-1.5 p-1.5">
+          Drivers Profile and Violation History
+        </Link>
+        <Link href="/violationReporting" className="m-1.5 p-1.5">
+          Violation and Reporting
+        </Link>
       </nav>
     </header>
   );
